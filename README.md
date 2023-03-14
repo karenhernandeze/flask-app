594ea2f68e2b6251daa44af00c1597c71dcf069a
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2023-03-13 19:20:18 -0600

    Missing Home Page

---
+++ README.md
@@ -0,0 +1,4 @@
+Run app using command 
+
+    docker run -d -p 5000:5000 python-docker
+
--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request, render_template 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello from Flask & Docker</h2>'
+def login_html():
+    return render_template("login.html")
 
+@app.route('/form_login', methods=['POST', 'GET'])
+def login(): 
+    username = request.form['username']
+    password = request.form['password']
+    checkpsswd = password.isalpha() or password.isnumeric()
+    
+    if username[0].isupper() and not(checkpsswd): 
+        return render_template('login.html', info='Correct')
+        # return render_template('home.html', name=username)
+    else:
+        return render_template('login.html', info='Invalid Credentials, Please Check Again')
 
-if __name__ == "__main__":
+    
+if __name__ == "__main__": 
     app.run(debug=True)
